2c78042bf2845f4a66854b52833335fa086a5556
Ichel Delgado <icheldmorales@gmail.com>
2023-08-13 19:24:46 -0500

    Ajustes sugeridos mileidys y build con cambios

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -4,6 +4,7 @@ import { MenuItem } from '../../models/menuItems';
 import AOS from "aos";
 import { PageScrollService } from 'ngx-page-scroll-core';
 
+const ITEM_IMPORTANTE = ["Contacto"];
 
 @Component({
   selector: 'app-navbar',
@@ -15,6 +16,8 @@ export class NavbarComponent implements OnInit{
   @Input()
   items: MenuItem[];
   imageToolbar:string;
+  itemsRegulares:MenuItem[] = [];
+  itemsImportante: MenuItem[] = [];
 
   constructor(private pageScrollService: PageScrollService,
               @Inject(DOCUMENT) private document: any){}
@@ -27,16 +30,36 @@ export class NavbarComponent implements OnInit{
     });
   }
 
-  ngOnInit(){
-    AOS.init();
+  private definirUbicacionImagen(){
     let ubicacionActual = window.location.href;
     console.log(ubicacionActual)
     if (ubicacionActual.indexOf("localhost") > 0 || ubicacionActual.indexOf("127.0.0.1") > 0){
-      this.imageToolbar = '../../../assets/image/bardelhaus_logo.png';
+      this.imageToolbar = '../../../assets/image/bardelhaus_logon.png';
     }
     else {
-      this.imageToolbar = "assets/image/bardelhaus_logo.png";
+      this.imageToolbar = "assets/image/bardelhaus_logon.png";
     }
+  }
+
+  private definirImportanciaItems(){
+    this.items.forEach(item => {
+      ITEM_IMPORTANTE.forEach(itemImportante => {
+        if (item.nombre != itemImportante){
+          this.itemsRegulares.push(item);
+        }
+        else {
+          this.itemsImportante.push(item);
+        }
+      });
+    });
+    console.log(this.itemsRegulares);
+    console.log(this.itemsImportante);
+  }
+
+  ngOnInit(){
+    AOS.init();
+    this.definirUbicacionImagen();
+    this.definirImportanciaItems();
   }
 
 }
